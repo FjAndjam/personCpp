1271880d6d1230d57e97f3454175e0bb1abe1011
Luis <704017396@qq.com>
2023-02-25 22:52:12 +0800

    test typename ...Types

--- exc/test/test.cpp
+++ exc/test/test.cpp
@@ -1,12 +1,34 @@
 
 #include <iostream>
+#include <bitset>
 
-int main(int argc, char **argv)
+void print(){};
+
+template <typename... Types>
+void print(const Types &...args)
 {
-	std::cout << argc << std::endl;
-	for(int i=0; i < argc; i++) {
-		// std::cout << argv[i] << std::endl;
-		printf("%s\n", argv[i]);
-	}
+	std::cout << "size of ...(args):" << sizeof...(args) << std::endl;
+}
+
+template <typename T, typename... Types>
+void print(const T &firstArg, const Types &...args)
+{
+	std::cout << firstArg << std::endl;
+	print(args...);
+}
+
+// int main(int argc, char **argv)
+int main()
+{
+	// std::cout << argc << std::endl;
+	// for (int i = 0; i < argc; i++)
+	// {
+	// 	// std::cout << argv[i] << std::endl;
+	// 	printf("%s\n", argv[i]);
+	// }
+	// std::cout << __cplusplus << std::endl;
+
+	print(7.5, "hello", std::bitset<16>(322), 43);
+
 	return 0;
 }
